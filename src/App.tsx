f4cab0d5c7e209fbd67887f8a45643b64ad7d5c8
Ryan Gammon <ryan@gamnation.net>
2020-10-11 10:23:20 -0700

    Add semantic ui and top navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Menu } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu attached='top'>
+        <Menu.Menu position='right'>
+          Login
+        </Menu.Menu>
+      </Menu>
     </div>
   );
 }
