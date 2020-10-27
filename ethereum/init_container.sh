2a63dcee351db902daf9dc265f62e79c008028ea
Ryan Gammon <ryan@gamnation.net>
2020-10-26 17:37:41 -0700

    Docker container for geth

---
+++ ethereum/init_container.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# See: https://ahmet.im/blog/minimal-init-process-for-containers/
+set -e
+
+if [ -d /mnt/rcfm/geth ]
+then
+    /usr/bin/geth \
+        --networkid 206 \
+        --ipcdisable \
+        --nodiscover \
+        --datadir /mnt/rcfm/geth/data \
+        --http \
+        --http.addr 0.0.0.0 \
+        --http.port 8545 \
+        --http.api eth,web3,personal,net \
+        --http.vhosts "*" \
+        --nousb \
+        --nat none \
+        --mine \
+        --unlock 0x2F37f4757138D9fa30307F341459b9531CCD9Ba6 \
+        --password /mnt/rcfm/geth/password.txt \
+        --allow-insecure-unlock &
+else
+    echo "Not running geth"
+    sleep 1d
+fi
+
+wait -n
